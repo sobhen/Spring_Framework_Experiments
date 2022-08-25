a1889ce64ff0a4a88d53934764a22e219676ca2e
sobhen <sobhen@gmail.com>
2022-08-25 07:41:38 +0530

    Initial Project and Docs

---
+++ springexp/src/main/java/com/springexp/SpringexpApplication.java
@@ -0,0 +1,13 @@
+package com.springexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringexpApplication.class, args);
+	}
+
+}
---
+++ springexp/src/test/java/com/springexp/SpringexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
